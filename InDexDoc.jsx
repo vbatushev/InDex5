ca5b675ceb2e823d060eb457cd1966775f1631e7
Vitaly Batushev <support@szam5.com>
2014-07-13 12:46:21 -0430

    InDexFind и исправление README
    Вынес обработку во внешний файл, добавил несколько строк в README

--- InDexDoc.jsx
+++ InDexDoc.jsx
@@ -1,74 +1,12 @@
-var indexver = 5.3;
-
-function trim (str, charlist) { var whitespace, l = 0, i = 0; str += ''; if (!charlist) { whitespace = " \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000"; } else { charlist += ''; whitespace = charlist.replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '$1'); } l = str.length; for (i = 0; i < l; i++) { if (whitespace.indexOf(str.charAt(i)) === -1) { str = str.substring(i); break; } } l = str.length; for (i = l - 1; i >= 0; i--) { if (whitespace.indexOf(str.charAt(i)) === -1) { str = str.substring(0, i + 1);break;}} return whitespace.indexOf(str.charAt(0)) === -1 ? str : ''; }
-
-Object.prototype.indexFind = function (findArray, workIndex, fStyles, fcStyles, caseSens) {
-
-	app.findGrepPreferences = null; app.changeGrepPreferences = null;
-	var parentObject = workIndex;
-	for (var fa = 0; fa < findArray.length; fa++) {
-
-		if (!caseSens) { var ss = "(?i)"; } else { var ss = "";}
-		
-		var findStrings = trim(findArray[fa]);
-		
-		var fss = findStrings.split('=>');
-		if (fss.length > 1) {
-			var topicName = fss[0];
-			var findString = fss[1];
-		} else {
-			var findString = findStrings;
-			var topicName = findStrings;
-		}
-		
-		findFindString = ss + findString;
-		if (parentObject.topics.itemByName(topicName).isValid) {
-			var parObject = parentObject.topics.itemByName(topicName);
-		} else {
-			var parObject = parentObject.topics.add(topicName);
-			app.findGrepPreferences.findWhat = findFindString;
-			var myFinds = this.findGrep(true);
-			app.findGrepPreferences = null; app.changeGrepPreferences = null;
-			if (myFinds) {
-				for (f = 0; f < myFinds.length; f++) {
-					myFinds[f].select(); var FindSelection = app.activeDocument.selection[0];
-					if (fStyles == '') {
-						for (cf = 0; cf < fcStyles.length; cf++) {
-							if (FindSelection.appliedCharacterStyle == fcStyles[cf] || fcStyles[cf] == "") { 
-								parObject.pageReferences.add(FindSelection, PageReferenceType.currentPage);
-							}
-						}
-					} else {
-						var test = false;
-						for (tf = 0; tf < fStyles.length; tf++) {
-							if (FindSelection.appliedParagraphStyle == fStyles[tf]) { test =true; }
-							for (cf = 0; cf < fcStyles.length; cf++) {
-								if (FindSelection.appliedCharacterStyle == fcStyles[cf] || fcStyles[cf] == "") { 
-									test =true;
-								} else {
-									test = false;
-								}
-							}
-						}
-						if (test) {
-							parObject.pageReferences.add(FindSelection, PageReferenceType.currentPage);
-						}
-					}
-				}
-			}
-		}
-
-		parentObject = parObject;
-	}
-}
+#include "InDexFind.jsxinc"
 
 if (app.documents.length == 0) {
-	alert("Нет открытых публикаций. Откройте публикацию \nи запустите скрипт еще раз.", "Ошибка: Нет открытых публикаций", true);  exit();
+	alert(docAlertText, docAlertHead, true);  exit();
 }
 
-var workfile = File.openDialog ('Выберите файл с данными', 'Text Files:*.txt', false); if (!workfile) { exit(); }
+var workfile = File.openDialog (docOpenDialogHead, 'Text Files:*.txt', false); if (!workfile) { exit(); }
 var res =  "dialog {\
-		text:' inDex 5 ', properties: { }, \
+		text:' InDex " + indexver +  " ', properties: { }, \
 		bigGroup:Group {orientation:'row', alignChildren:['fill','fill'], \
 			fGroup:Group {orientation:'column', alignChildren:['fill','top'], \
 				tpPanel:Panel {orientation:'row', alignChildren:['fill', 'fill'], margins:[10,16,10,10], text:'Old Topics in Index', \
