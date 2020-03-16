b90b495800271828e84c6a79cbe6c9dd09fbd955
chevere <vitaly@batushev.info>
2020-03-16 19:37:36 +0300

    Убрал закомментированный код

--- InDexBook.jsx
+++ InDexBook.jsx
@@ -3,22 +3,6 @@
     #include "InDexFind.jsxinc"
 
     var main = function() {
-        // if (!app.documents.length && !!app.books.length) {
-        //     var bookstyledoc = app.open(app.activeBook.bookContents[0].fullName);
-        // } else {
-            // if (!!app.documents.length && !!app.books.length) {
-            //     alert("Close all open documents\nand run script again.", "Error: open documents.", true);
-            //     exit();
-            // }
-            // if (!!app.documents.length && !app.books.length) {
-            //     alert("Not opened books for process.\nClose all open documents,\nopen book and run script again.", "Error: has opened documents and has not opened book", true);
-            //     exit();
-            // }
-            // if (!app.documents.length && !app.books.length) {
-            //     alert("Нет открытых книг для обработки.\nОткройте книгу и запустите скрипт еще раз.", "Ошибка: Не открыта книга", true);
-            //     exit();
-            // }
-        // }
         if (!app.books.length) {
             alert("Not opened books for process.\nOpen book and run script again.", "Error: has not opened book", true);
             exit();
